4751fe84c57da688d2d8d4f8a2ab7f3e8330a2e4
mrinal2100 <mrinals2100@gmail.com>
2025-09-14 13:12:49 -0400

    updated greeting message

--- express-app/index.js
+++ express-app/index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World.patna</h1>')
+  res.send('<h1>Hello World.JAMMU</h1>')
 })
 
 const PORT = 8080
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0' ,() => {
   console.log(`Server running on port ${PORT}`)
 })
